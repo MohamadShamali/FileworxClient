80cafee98c028b9838793f0a0878aa8a503431f6
Mohamed AL-Shamali <M.AL-Shamali@sedco-online.com>
2023-08-20 11:45:43 +0300

    -

--- WebFileworxClient/EditBeforeRun.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Web_Fileworx_Client
-{
-    public class EditBeforeRun
-    {
-        //Directorys of the Program Saved files
-        public const string ImagesFiles = @"C:\Users\M.AL-Shamali\Desktop\Demo Projects\Web Fileworx Client\WebFileworxClient\Web Fileworx Client\wwwroot\Images\StoredImages";
-    }
-}
