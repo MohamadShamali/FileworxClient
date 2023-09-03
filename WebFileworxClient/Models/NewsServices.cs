2a71afd11af7b58c97f1a15b8471ae8931882ccd
Mohamed AL-Shamali <M.AL-Shamali@sedco-online.com>
2023-09-03 11:57:40 +0300

    Blazor-App: An option in the UI makes the user select either DB or ES

--- WebFileworxClient/Models/NewsServices.cs
+++ WebFileworxClient/Models/NewsServices.cs
@@ -13,16 +13,16 @@ namespace Web_Fileworx_Client.Models
         public List<clsPhoto> AllPhotos = new List<clsPhoto>();
         public List<clsFile> AllFiles = new List<clsFile>();
         public clsFile? SelectedFile { get; set; }
-        private QuerySource querySource { get; set; } = QuerySource.ES;
+        public QuerySource QuerySource { get; set; } = QuerySource.ES;
 
         public async Task AddDBFilesToFilesList()
         {
             clsNewsQuery allNewsQuery = new clsNewsQuery();
-            allNewsQuery.Source = querySource;
+            allNewsQuery.Source = QuerySource;
             AllNews = await allNewsQuery.Run();
 
             clsPhotoQuery allPhotosQuery = new clsPhotoQuery();
-            allPhotosQuery.Source = querySource;
+            allPhotosQuery.Source = QuerySource;
             AllPhotos = await allPhotosQuery.Run();
 
 
@@ -31,9 +31,9 @@ namespace Web_Fileworx_Client.Models
             AllFiles.AddRange(AllNews);
         }
 
-        public void RefreshFilesList()
+        public async Task RefreshFilesList()
         {
-            AddDBFilesToFilesList();
+            await AddDBFilesToFilesList();
         }
 
     }
--- WebFileworxClient/Models/UsersServices.cs
+++ WebFileworxClient/Models/UsersServices.cs
@@ -7,18 +7,18 @@ namespace Web_Fileworx_Client.Models
         public List<clsUser> AllUsers = new List<clsUser>();
         public clsUser LoggedInUser { get; set; } = null;
         public clsUser SelectedUser { get; set; } = null;
-        private QuerySource querySource { get; set; } = QuerySource.ES;
+        public QuerySource QuerySource { get; set; } = QuerySource.ES;
 
         public async Task AddDBUsersToUsersList()
         {
             clsUserQuery allUsersQuery = new clsUserQuery();
-            allUsersQuery.Source = querySource;
+            allUsersQuery.Source = QuerySource;
             AllUsers = await allUsersQuery.Run();
         }
 
-        public void RefreshUsersList()
+        public async Task RefreshUsersList()
         {
-            AddDBUsersToUsersList();
+            await AddDBUsersToUsersList();
         }
 
     }
